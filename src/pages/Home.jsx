39ea7b72bfeb5624a796d0e2d9d84c2cc0242494
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-10-31 20:54:29 +0200

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+// component
+import UploadImage from "../components/UploadImage";
+
+const Home = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+
+  return (
+    <div className="row justify-content-center align-items-center">
+      <div className="col-10 col-sm-8 col-md-5 col-lg-4">
+        <UploadImage setImageUrls={setImageUrls} />
+      </div>
+      {imageUrls.length !== 0 ? (
+        <div className="mt-4 col-10">
+          <h2 className="text-center fw-bold">
+            The image that storage in the firebase
+          </h2>
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            {imageUrls.map((url, index) => {
+              return (
+                <div
+                  className="w-100 d-flex justify-content-center align-items-center"
+                  height={"200px"}
+                  key={index}
+                >
+                  <img
+                    src={url}
+                    alt="storage url from firebase"
+                    className="rounded image-upload"
+                    style={{ height: "100px" }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Home;
